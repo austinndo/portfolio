9f091416102c642431e7e3d85bbbd42aee53272c
Austin Do <austinndo@gmail.com>
2022-11-01 12:57:30 -0700

    Changed some of the text in the about me section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,11 +8,12 @@ const AboutSection = ({ id }) => {
         <div className="AboutSplitOne">
           <div className="AboutParagraphOne">
             <p>
-              Hi, I'm Austin Do. I'm a software engineer with a background in
-              biology and analytics. I first began coding in college where I
-              used <span>R</span> to analyze biological data. After graduation I
-              joined a biotech startup where I worked cross-functionally with
-              Sales, Software, and Operations.
+              I enjoy using my background in biology and analytics to solve
+              challenging problems as a software engineer. My first experience
+              with coding began in college where I used <span>R</span> to
+              analyze biological data. After graduation I joined a biotech
+              startup where I worked cross-functionally with Sales, Software,
+              and Operations.
             </p>
           </div>
 
@@ -28,14 +29,10 @@ const AboutSection = ({ id }) => {
             In my role as a planner at this startup I helped develop a{' '}
             <span>Python</span> algorithm with our Dev Ops team to automate
             order processing. It was at this time that I fully realized my
-            interest in software engineering.
-          </p>
-
-          <p>
-            I'm excited to continue developing my software engineering skills!
-            Most recently I've worked with <span>Javascript</span> to create a
-            handful of full stack <span>React</span> apps. Check out a few of my
-            projects below!
+            interest in software engineering. I'm excited to continue developing
+            my skills! Most recently I've worked with <span>Javascript</span> to
+            create a handful of full stack <span>React</span> apps. Check out a
+            few of my projects below!
           </p>
         </div>
       </div>
--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -5,14 +5,16 @@ const IntroSection = ({ id }) => {
     <div className="SectionContent" id={id}>
       <div className="IntroSection">
         <h2>Hello!</h2>
-        <h2>My name is Austin Do</h2>
-        <h4>
-          I'm a solutions focused software engineer with experience developing
-          and managing LIMS applications. My involvement at the intersection of
-          operations and sales inspired me to continuously improve user systems
-          and has taught me to value open communication and active
-          collaboration.
-        </h4>
+        <h2>My name is Austin Do,</h2>
+        <div className="IntroParagraph">
+          <p>
+            I'm a solutions focused software engineer with experience developing
+            and managing LIMS applications. My involvement at the intersection
+            of operations and sales inspired me to continuously improve user
+            systems and has taught me to value open communication and active
+            collaboration.
+          </p>
+        </div>
 
         <h3>Resume</h3>
         <a href={resume} download>
