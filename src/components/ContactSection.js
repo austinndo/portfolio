05847794c6f1627653db6d9216092c63cd21e681
Austin Do <austinndo@gmail.com>
2022-11-10 12:13:09 -0800

    Completed sizing and format. Working on color and background

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,29 +1,26 @@
-import resume from '../assets/Austin_Do_Resume.docx.pdf'
 import { LinkedinOriginalIcon, GithubOriginalIcon } from 'react-devicons'
-import { HiDocumentDownload } from 'react-icons/hi'
-import { MdOutlineLaunch, MdOutlineEmail } from 'react-icons/md'
+import { MdOutlineEmail } from 'react-icons/md'
 
 const ContactSection = ({ id }) => {
   return (
     <div className="SectionContent" id={id}>
       <div className="ContactSection">
         <p className="ContactTitle">Contact Me</p>
-
+        <div className="ContactEmailContainer">
+          <p>Send me a message!</p>
+          <p
+            className="email-link"
+            onClick={() => {
+              window.location = 'mailto:austinndo@gmail.com'
+            }}
+          >
+            <MdOutlineEmail size={42} />
+          </p>
+          {/* <p className="email-text">austinndo@gmail.com</p> */}
+        </div>
         <div className="ContactSocialsContainer">
+          <p>Connect with me:</p>
           <ul className="ContactLinks">
-            <li>
-              <div className="ContactLinkContainer">
-                <p
-                  className="email-link"
-                  onClick={() => {
-                    window.location = 'mailto:austinndo@gmail.com'
-                  }}
-                >
-                  <MdOutlineEmail size={42} />
-                </p>
-                <p>austinndo@gmail.com</p>
-              </div>
-            </li>
             <li>
               <div className="ContactLinkContainer">
                 <a
@@ -33,7 +30,6 @@ const ContactSection = ({ id }) => {
                 >
                   <LinkedinOriginalIcon size={42} />
                 </a>
-                <p>linkedin.com/in/austin-do/</p>
               </div>
             </li>
             <li>
@@ -45,7 +41,6 @@ const ContactSection = ({ id }) => {
                 >
                   <GithubOriginalIcon size={42} />
                 </a>
-                <p>github.com/austinndo</p>
               </div>
             </li>
           </ul>
